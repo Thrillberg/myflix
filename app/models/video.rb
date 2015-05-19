2d478754aa287e0bbe0bfda97e082ce479daed7a
Eric Tillberg <eric.tillberg@gmail.com>
2015-05-19 13:59:14 -0400

    partway through week 2

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,11 +1,11 @@
 class Video < ActiveRecord::Base
   belongs_to :category
-  has_many :reviews
+  has_many :reviews, -> { order(created_at: :desc) }
 
   validates_presence_of :title, :description
 
   def self.search_by_title(search_term)
     return [] if search_term.blank?
-    where("title LIKE ?","%#{search_term}%").order("created_at DESC")
+    where("title LIKE ?","%#{search_term}%").order(created_at: :desc)
   end
 end
