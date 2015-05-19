660f89ade2396ef79aec19e4922630477026b19d
Eric Tillberg <eric.tillberg@gmail.com>
2015-05-19 11:59:19 -0400

    fixed strong params

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
   def create
     video = Video.find(params[:video_id])
-    video.reviews.create(params[:review].merge!(user: current_user))
+    video.reviews.create(review_params.merge!(user: current_user))
     redirect_to video
   end
 
