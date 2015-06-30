617992548ee4986d355b75e1af8a1d23aebbcaad
Eric Tillberg <eric.tillberg@gmail.com>
2015-06-30 13:14:05 -0400

    trying to use unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,16 @@
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
+
+before_fork do |server, worker|
+   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
